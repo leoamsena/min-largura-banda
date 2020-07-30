14e59aa11c8299ce19747893c3f648a27abca398
luizosoares <luizotavio@ioasys.com.br>
2020-07-30 16:48:22 -0300

    changes

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-const initialSolution = (A, f) => {
+const initialSolution = (A) => {
   const len_graph = A.length;
+
+  let f = new Array(len_graph);
+
   let q = new Array(len_graph);
   q = q.map(() => 0);
 
@@ -8,15 +11,15 @@ const initialSolution = (A, f) => {
   let i = 1;
   let mark = Array(len_graph);
   mark = mark.map(() => false);
-  let k = Math.random() * (len_graph - 1);
+  let k = Math.round(Math.random() * (len_graph - 1));
   mark[k] = true;
   q[0] = k;
   let ql = 1;
   f[k] = 0;
   let l = 0;
-  while (l < len_graph) {
+  while (l < len_graph + 1) {
     let r = 0;
-    for (let i1 = 0; i1 < ql + 1; i += 1) {
+    for (let i1 = 0; i1 < ql; i1 += 1) {
       i = q[i1];
       for (let j1 = 0; j1 < A[i].length; j1 += 1) {
         let j = A[i][j1];
@@ -27,7 +30,7 @@ const initialSolution = (A, f) => {
         }
       }
     }
-    let j_ = MATH.random(ql);
+    let j_ = Math.round(Math.random() * ql);
     for (let jl = 0; jl < ql + 1; jl += 1) {
       let j = q[j_];
       l += 1;
@@ -35,16 +38,24 @@ const initialSolution = (A, f) => {
       j_ += 1;
       if (j_ >= ql - 1) j_ = 0;
     }
-    q = s;
+    const newVet = [];
+    for (let aux = 0; aux < len_graph; aux += 1) {
+      if (q[aux] !== undefined) newVet[aux] = q[aux];
+      if (s[aux] !== undefined) newVet[aux] = s[aux];
+      if (q[aux] === undefined && s[aux] === undefined) newVet[aux] = undefined;
+    }
+    q = newVet;
     ql = r;
   }
-}
-A = [
+
+  console.log(f);
+};
+const A = [
   [1, 0, 1, 0, 1],
   [0, 1, 1, 1, 0],
   [1, 1, 1, 0, 1],
   [0, 1, 0, 1, 0],
   [1, 0, 1, 0, 1],
 ];
-f = [];
+const f = [];
 initialSolution(A, f);
